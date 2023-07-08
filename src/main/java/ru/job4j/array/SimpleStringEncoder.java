b57e9062abb77f08a446bc427988a92d020fd401
andrey <larginator@gmail.com>
2023-07-08 14:28:28 +0600

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -3,13 +3,11 @@ package ru.job4j.array;
 public class SimpleStringEncoder {
     public static String encode(String input) {
         StringBuilder result = new StringBuilder();
-        char currentSymbol;
-        boolean isLastSymbol;
+        char currentSymbol = '\0';
         int counter = 1;
-        for (int i = 0; i < input.length(); i++) {
-            isLastSymbol = i == input.length() - 1;
+        for (int i = 0; i < input.length() - 1; i++) {
             currentSymbol = input.charAt(i);
-            if (isLastSymbol || currentSymbol != input.charAt(i + 1)) {
+            if (currentSymbol != input.charAt(i + 1)) {
                 result.append(currentSymbol);
                 if (counter > 1) {
                     result.append(counter);
@@ -18,6 +16,16 @@ public class SimpleStringEncoder {
             } else {
                 counter++;
             }
+        }
+
+        char lastSymbol = input.charAt(input.length() - 1);
+        if (currentSymbol == lastSymbol) {
+            result.append(currentSymbol);
+            if (counter > 1) {
+                result.append(counter);
+            }
+        } else {
+            result.append(lastSymbol);
         }
 
         return result.toString();
