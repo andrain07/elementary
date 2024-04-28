99ba9bf46c2371d9d88655e457c57365cd167eee
larginator <larginator@gmail.com>
2024-04-28 20:54:25 +0600

    Codium. Нейросети спешат на помощь. [#505152]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -2,6 +2,9 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал отрицательного числа не определен");
+        }
         int result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
